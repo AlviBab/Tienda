eb7e92e6e77ffd16550f626e2514859c23d069b3
Alvaro Babilonia <babiloniaalvaro39@gmail.com>
2022-04-03 21:01:02 -0500

    Agregando PHP al proyecto

---
+++ login.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/e700d617b6.js" crossorigin="anonymous"></script>
+        
+    <title>Fande</title>
+</head>
+<body>
+    <style>
+        body{
+            background-color: grey;
+        }
+    </style>
+    <div class="bg-header"><!--Cabecera de la pagina-->
+        <header class="header " >
+            <div class="logo">
+                <a href="index.html"><h1 class="no-margin no-padding">Shop</h1></a>
+            </div>
+            <nav class="navegacion-principal">
+                <a href="#" class="navegacion-principal__items margin hover">Tienda</a>
+                <a href="#" class="navegacion-principal__items margin hover">Nosotros</a>
+                <a href="#" class="navegacion-principal__items margin hover">Contacto</a>
+                <a href="#" class="navegacion-principal__items margin hover">Ayuda</a>
+                <div class="utilidades">
+                    <a href="#" class="navegacion-principal__items carrito "><i class="fa-solid fa-location-dot"></i></a>
+                    <a href="#" class="navegacion-principal__items carrito "><i class="fa-solid fa-heart"></i></a>
+                    <a href="#" class="navegacion-principal__items carrito "><i class="fa-solid fa-cart-shopping"></i></a>
+                </div>
+                <a href="#" class="navegacion-principal__items login">Log in <i class="fa-solid fa-user"></i></a>
+            </nav>
+            <div class="end-header ">
+                    <a href="#" class="navegacion-principal__items carrito2 "><i class="fa-solid fa-location-dot"></i></a>
+                    <a href="#" class="navegacion-principal__items carrito2 "><i class="fa-solid fa-heart"></i></a>
+                    <a href="#" class="navegacion-principal__items carrito2 "><i class="fa-solid fa-cart-shopping"></i></a>
+                    <a href="#" class="navegacion-principal__items login2 "><i class="fa-solid fa-user"></i></a>
+                    <i id="btnAbrir" class="fa-solid fa-bars barra"></i>
+                 
+            </div>
+        </header>
+    </div><!--EndCabezera-->
+    <main class="main-login"><!--Cuerpo Principal-->
+        <div class="imagen">
+            <img src="asests/img/gorra.png" alt="Nuestro servicios">
+        </div>
+        <div class="wraper">
+            <div class="title"><span>Login Fande</span></div>
+            <form action="#">
+                <div class="metodos-login">
+                    <button> <img src="asests/img/icono-google.png" width="22px"
+                        height="22px" alt="icono de google"> Google</button>
+                    <button><img src="asests/img/icono-facebook.png" width="22px"
+                        height="22px" alt="icono de Facebook"></i> Facebook</button>
+                    
+                </div>
+                <div class="row">
+                    <i class="fas fa-user"></i>
+                    <input type="email" placeholder="Email o Telefono" required maxlength="30">
+                </div><!--EndRow-->
+                <div class="row">
+                    <i class="fas fa-lock"></i>
+                    <input type="password" placeholder="Contraseña"  required maxlength="16">
+                </div><!--EndRow-->
+                <div class="pass"><a class="color-primario" href="#">¿Olvidaste la contraseña?</a></div>
+                <div class="row buttom">
+                    <input type="submit" value="Login">
+                </div><!--EndRow-->
+                <div class="signup-link">¿No estas registrado? <a class="color-primario" href="registro.html">Registrate ahora</a></div>
+            </form>
+        </div> 
+    </main><!--EndMain-->
+
+    <footer><!--Fin-->
+
+    </footer>
+    <script src="js/app.js"></script>
+</body>
+</html>
---
+++ registro.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/registro.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/e700d617b6.js" crossorigin="anonymous"></script>
+        
+    <title>Fande</title>
+</head>
+<body>
+    <style>
+        body{
+            background-color: grey;
+        }
+    </style>
+    <div class="bg-header"><!--Cabecera de la pagina-->
+        <header class="header " >
+            <div class="logo">
+                <a href="index.html"><h1 class="no-margin no-padding">Shop</h1></a>
+            </div>
+            <nav class="navegacion-principal">
+                <a href="#" class="navegacion-principal__items margin hover">Tienda</a>
+                <a href="#" class="navegacion-principal__items margin hover">Nosotros</a>
+                <a href="#" class="navegacion-principal__items margin hover">Contacto</a>
+                <a href="#" class="navegacion-principal__items margin hover">Ayuda</a>
+                <div class="utilidades">
+                    <a href="#" class="navegacion-principal__items carrito "><i class="fa-solid fa-location-dot"></i></a>
+                    <a href="#" class="navegacion-principal__items carrito "><i class="fa-solid fa-heart"></i></a>
+                    <a href="#" class="navegacion-principal__items carrito "><i class="fa-solid fa-cart-shopping"></i></a>
+                </div>
+                <a href="login.html" class="navegacion-principal__items login">Log in <i class="fa-solid fa-user"></i></a>
+            </nav>
+            <div class="end-header ">
+                    <a href="#" class="navegacion-principal__items carrito2 "><i class="fa-solid fa-location-dot"></i></a>
+                    <a href="#" class="navegacion-principal__items carrito2 "><i class="fa-solid fa-heart"></i></a>
+                    <a href="#" class="navegacion-principal__items carrito2 "><i class="fa-solid fa-cart-shopping"></i></a>
+                    <a href="login.html" class="navegacion-principal__items login2 "><i class="fa-solid fa-user"></i></a>
+                    <i id="btnAbrir" class="fa-solid fa-bars barra"></i>
+                 
+            </div>
+        </header>
+    </div><!--EndCabezera-->
+   
+    <main class="main-registro"><!--Cuerpo Principal-->
+        <div class="wraper">
+            <div class="title"><span>Registro Fande</span></div>
+            <form action="#">
+                <div class="metodos-registro">
+                    <button> <img src="asests/img/icono-google.png" width="22px"
+                        height="22px" alt="icono de google"> Google</button>
+                    <button><img src="asests/img/icono-facebook.png" width="22px"
+                        height="22px" alt="icono de Facebook"></i> Facebook</button>
+                    
+                </div>
+                <div class="row">
+               
+                    <input id="nombres" class="datos__users"  type="text" placeholder="Nombres" required maxlength="30">
+                </div><!--EndRow-->
+                <div class="row">
+                
+                    <input id="apellidos" class="datos__users" type="text" placeholder="Apellidos" required maxlength="30">
+                </div><!--EndRow-->
+                <div class="row">
+    
+                    <input id="edad" class="datos__users" type="number" placeholder="Edad" required maxlength="3">
+                </div><!--EndRow-->
+                <div class="row">
+                    <i class="fas fa-user"></i>
+                    <input id="emailTelefono" class="cuenta__users" type="email" placeholder="Email o Telefono" required maxlength="30">
+                </div><!--EndRow-->
+                <div class="row">
+                    <i class="fas fa-lock"></i>
+                    <input id="contraseña" class="cuenta__users"  type="password" placeholder="Contraseña"  required maxlength="16">
+                </div><!--EndRow-->
+                <div class="row">
+                    <i class="fas fa-lock"></i>
+                    <input id="checkContraseña" class="cuenta__users" type="password" placeholder="Repita Contraseña"  required maxlength="16">
+                </div><!--EndRow-->
+               
+                <div class="row buttom">
+                    <input id="registrar" class="" type="submit" value="registro">
+                </div><!--EndRow-->
+                <div class="signup-link">¿Ya estas registrado? <a class="color-primario" href="login.html">Inicia ahora</a></div>
+            </form>
+        </div> 
+    </main><!--EndMain-->
+    <footer><!--Fin-->
+
+    </footer>
+    <script src="js/app.js"></script>
+</body>
+</html>

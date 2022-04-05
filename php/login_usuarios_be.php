ddaac9d6bc2458e673d9823daad9dd6a76f19da0
Alvaro Babilonia <babiloniaalvaro39@gmail.com>
2022-04-04 22:43:19 -0500

    Trabajando en el bakend de login

--- php/login_usuarios_be.php
+++ php/login_usuarios_be.php
@@ -2,8 +2,24 @@
 
 include 'conexion_be.php' ;
 
-$usuario = $_POST ['usuario']
-$contrasena = $_POST ['contrasena']
+$usuario = $_POST ['usuario'];
+$contrasena = $_POST ['contrasena'];
 
+$query = "SELECT * FROM usuarios WHERE emailTelefono = '$usuario' AND contrasena = '$contrasena'";
+
+$validar_login = mysqli_query($conexion,$query);
+
+if (mysqli_num_rows($validar_login) > 0) {
+    header ("location: ../admin.php");
+    exit();
+}else{
+    echo '
+        <script>
+            alert("Usuario o contraseña incorecta")
+            window.location = "../login.php"
+        <script>
+    ';
+    exit();
+}
 
 ?>
